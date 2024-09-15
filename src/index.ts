dfdc2e38ca66c185c0d134dcee73dbfa49bf2ef9
Mario Garrido <>
2024-09-15 10:09:13 +0200

    chore: update vite.config & package

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import Button from './components/ui/button/Button.vue'
+
+export { Button }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), dts(), vueDevTools()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'MGVVueUIComponents',
+      fileName: 'mgv-vue-ui-components'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
